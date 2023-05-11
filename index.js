dc7c82b065b5e5ff4801fdf0e3db2844c7cd01f1
Connor Hubbard <connor@dominantstrategies.io>
2023-05-11 15:48:26 -0500

    out with the fancy in with the dirty

--- index.js
+++ index.js
@@ -1,81 +1,81 @@
 const Promise = require('bluebird')
-const { JsonRpcProvider, Wallet } = require('quais')
+const {JsonRpcProvider, Wallet} = require('quais')
 const walletsJson = require('./wallets.json')
-const { post } = require('axios')
+const {post} = require('axios')
 
 const winston = require('winston')
 
-const customFormat = winston.format.printf(({ level, message, timestamp, ...metadata }) => {
-  let msg = `${level.toUpperCase()} [${formatDate(new Date(timestamp))}] ${message} `
-  for (const key in metadata) {
-    msg += `${key}:= ${metadata[key]} `
-  }
-  return msg
+const customFormat = winston.format.printf(({level, message, timestamp, ...metadata}) => {
+    let msg = `${level.toUpperCase()} [${formatDate(new Date(timestamp))}] ${message} `
+    for (const key in metadata) {
+        msg += `${key}:= ${metadata[key]} `
+    }
+    return msg
 })
 
-function formatDate (date) {
-  const pad = (num) => num.toString().padStart(2, '0')
-  const padMilliseconds = (num) => num.toString().padStart(3, '0')
+function formatDate(date) {
+    const pad = (num) => num.toString().padStart(2, '0')
+    const padMilliseconds = (num) => num.toString().padStart(3, '0')
 
-  const day = pad(date.getDate())
-  const month = pad(date.getMonth() + 1)
-  const hours = pad(date.getHours())
-  const minutes = pad(date.getMinutes())
-  const seconds = pad(date.getSeconds())
-  const milliseconds = padMilliseconds(date.getMilliseconds())
+    const day = pad(date.getDate())
+    const month = pad(date.getMonth() + 1)
+    const hours = pad(date.getHours())
+    const minutes = pad(date.getMinutes())
+    const seconds = pad(date.getSeconds())
+    const milliseconds = padMilliseconds(date.getMilliseconds())
 
-  return `${month}-${day}|${hours}:${minutes}:${seconds}.${milliseconds}`
+    return `${month}-${day}|${hours}:${minutes}:${seconds}.${milliseconds}`
 }
 
 const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.ms(),
-    customFormat
-  ),
-  transports: [
-    new winston.transports.Console()
-  ]
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        customFormat
+    ),
+    transports: [
+        new winston.transports.Console()
+    ]
 })
 
 const nodeData = {
-  'zone-0-0': {
-    http: 8610,
-    ws: 8611
-  },
-  'zone-0-1': {
-    http: 8542,
-    ws: 8643
-  },
-  'zone-0-2': {
-    http: 8674,
-    ws: 8675
-  },
-  'zone-1-0': {
-    http: 8512,
-    ws: 8613
-  },
-  'zone-1-1': {
-    http: 8544,
-    ws: 8645
-  },
-  'zone-1-2': {
-    http: 8576,
-    ws: 8677
-  },
-  'zone-2-0': {
-    http: 8614,
-    ws: 8615
-  },
-  'zone-2-1': {
-    http: 8646,
-    ws: 8647
-  },
-  'zone-2-2': {
-    http: 8678,
-    ws: 8679
-  }
+    'zone-0-0': {
+        http: 8610,
+        ws: 8611
+    },
+    'zone-0-1': {
+        http: 8542,
+        ws: 8643
+    },
+    'zone-0-2': {
+        http: 8674,
+        ws: 8675
+    },
+    'zone-1-0': {
+        http: 8512,
+        ws: 8613
+    },
+    'zone-1-1': {
+        http: 8544,
+        ws: 8645
+    },
+    'zone-1-2': {
+        http: 8576,
+        ws: 8677
+    },
+    'zone-2-0': {
+        http: 8614,
+        ws: 8615
+    },
+    'zone-2-1': {
+        http: 8646,
+        ws: 8647
+    },
+    'zone-2-2': {
+        http: 8678,
+        ws: 8679
+    }
 }
 
 const host = 'localhost'
@@ -91,244 +91,240 @@ const etxFreq = 0.2
 const provider = new JsonRpcProvider(providerUrl)
 const memPoolMax = 9000
 
-let memPoolSize; let transactions = 0
+let memPoolSize;
+let transactions = 0
 let latest
 let interval = 4000
 let desiredTps = 40 / 6
 let feeData
-async function sendRawTransaction (url, signedHexValue) {
-  try {
-    const result = await post(url, {
-      jsonrpc: '2.0',
-      method: 'quai_sendRawTransaction',
-      params: [signedHexValue],
-      id: 1
-    })
-    if (result.data.error) {
-      logger.error('Error1: ', result.data.error)
+
+async function sendRawTransaction(url, signedHexValue) {
+    try {
+        const result = await post(url, {
+            jsonrpc: '2.0',
+            method: 'quai_sendRawTransaction',
+            params: [signedHexValue],
+            id: 1
+        })
+        if (result.data.error) {
+            logger.error('Error1: ', result.data.error)
+        } else {
+            transactions++
+        }
+    } catch (error) {
+        logger.error('Error2: ', error.message)
+    }
+}
+
+async function lookupTxPending(url) {
+    try {
+        const result = await post(url, {
+            jsonrpc: '2.0',
+            method: 'txpool_status',
+            id: 1
+        })
+        if (result.data.error) {
+            logger.error('Error1: ', result.data.error)
+            return 0
+        }
+        const resPend = result.data.result.pending
+        const resQueued = result.data.result.queued
+        return [Number(resPend), Number(resQueued)]
+    } catch (error) {
+        logger.error('Error2: ', error)
+        return 0
+    }
+}
+
+async function genRawTransaction(wallet, nonce) {
+    const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
+    const isExternal = Math.random() < etxFreq
+
+    let to, type
+
+    if (isExternal) { // is external this time
+        to = getRandomExternalAddress()
+        type = 2
     } else {
-      transactions++
+        to = getRandomInternalAddress()
+        type = 0
     }
-  } catch (error) {
-    logger.error('Error2: ', error.message)
-  }
-}
 
-async function lookupTxPending (url) {
-  try {
-    const result = await post(url, {
-      jsonrpc: '2.0',
-      method: 'txpool_status',
-      id: 1
-    })
-    if (result.data.error) {
-      logger.error('Error1: ', result.data.error)
-      return 0
+    const ret = {
+        to,
+        value,
+        nonce,
+        gasLimit: 42000,
+        maxFeePerGas: feeData.maxPriorityFeePerGas * BigInt(2),
+        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+        type,
+        chainId
     }
-    const resPend = result.data.result.pending
-    const resQueued = result.data.result.queued
-    return [Number(resPend), Number(resQueued)]
-  } catch (error) {
-    logger.error('Error2: ', error)
-    return 0
-  }
+    if (isExternal) { // is external this time
+        ret.externalGasLimit = BigInt(100000)
+        ret.externalGasPrice = BigInt(
+            Number(feeData.maxFeePerGas) * 2
+        )
+        ret.externalGasTip = BigInt(
+            Number(feeData.maxPriorityFeePerGas) * 2
+        )
+    }
+    return ret
 }
 
-async function genRawTransaction (wallet, nonce) {
-  const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
-  const isExternal = Math.random() < etxFreq
-
-  let to, type
-
-  if (isExternal) { // is external this time
-    to = getRandomExternalAddress()
-    type = 2
-  } else {
-    to = getRandomInternalAddress()
-    type = 0
-  }
-
-  const ret = {
-    to,
-    value,
-    nonce,
-    gasLimit: 42000,
-    maxFeePerGas: feeData.maxPriorityFeePerGas * BigInt(2),
-    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
-    type,
-    chainId
-  }
-  if (isExternal) { // is external this time
-    ret.externalGasLimit = BigInt(100000)
-    ret.externalGasPrice = BigInt(
-      Number(feeData.maxFeePerGas) * 2
-    )
-    ret.externalGasTip = BigInt(
-      Number(feeData.maxPriorityFeePerGas) * 2
-    )
-  }
-  return ret
-}
-
-function sleep (s) {
-  return new Promise((resolve) => setTimeout(resolve, s))
+function sleep(s) {
+    return new Promise((resolve) => setTimeout(resolve, s))
 }
 
 // Generate a random Ethereum address
-function generateRandomAddress () {
-  const hexChars = '0123456789abcdef'
-  let address = '0x'
+function generateRandomAddress() {
+    const hexChars = '0123456789abcdef'
+    let address = '0x'
 
-  // Generate 40 random hexadecimal characters
-  for (let i = 0; i < 40; i++) {
-    const randomIndex = Math.floor(Math.random() * 16)
-    address += hexChars[randomIndex]
-  }
+    // Generate 40 random hexadecimal characters
+    for (let i = 0; i < 40; i++) {
+        const randomIndex = Math.floor(Math.random() * 16)
+        address += hexChars[randomIndex]
+    }
 
-  return address
+    return address
 }
 
-function getRandomAddressInShard (shard) {
-  const start = Number(shard.byte[0])
-  const end = Number(shard.byte[1])
-  let prefix = Math.floor(Math.random() * (end - start + 1) + start).toString(16)
-  // if prefix is only 1 character, add a 0 to the front
-  if (prefix.length === 1) {
-    prefix = '0' + prefix
-  }
-  let randomAddress = generateRandomAddress()
-  // replace first 4 characters with random number between start and end
-  randomAddress = randomAddress.replace(
-    randomAddress.substring(2, 4),
-    prefix
-  )
-  return randomAddress
+function getRandomAddressInShard(shard) {
+    const start = Number(shard.byte[0])
+    const end = Number(shard.byte[1])
+    let prefix = Math.floor(Math.random() * (end - start + 1) + start).toString(16)
+    // if prefix is only 1 character, add a 0 to the front
+    if (prefix.length === 1) {
+        prefix = '0' + prefix
+    }
+    let randomAddress = generateRandomAddress()
+    // replace first 4 characters with random number between start and end
+    randomAddress = randomAddress.replace(
+        randomAddress.substring(2, 4),
+        prefix
+    )
+    return randomAddress
 }
 
-function getRandomExternalAddress () {
-  return getRandomAddressInShard(externalShards[Math.floor(Math.random() * externalShards.length)])
+function getRandomExternalAddress() {
+    return getRandomAddressInShard(externalShards[Math.floor(Math.random() * externalShards.length)])
 }
 
-function getRandomInternalAddress () {
-  return getRandomAddressInShard(selectedShard)
+function getRandomInternalAddress() {
+    return getRandomAddressInShard(selectedShard)
 }
 
 const QUAI_CONTEXTS = [
-  {
-    name: 'Cyprus One',
-    shard: 'zone-0-0',
-    context: 2,
-    byte: ['0x00', '0x1D']
-  },
-  {
-    name: 'Cyprus Two',
-    shard: 'zone-0-1',
-    context: 2,
-    byte: ['0x1E', '0x3A']
-  },
-  {
-    name: 'Cyprus Three',
-    shard: 'zone-0-2',
-    context: 2,
-    byte: ['0x3B', '0x57']
-  },
-  {
-    name: 'Paxos One',
-    shard: 'zone-1-0',
-    context: 2,
-    byte: ['0x58', '0x73']
-  },
-  {
-    name: 'Paxos Two',
-    shard: 'zone-1-1',
-    context: 2,
-    byte: ['0x74', '0x8F']
-  },
-  {
-    name: 'Paxos Three',
-    shard: 'zone-1-2',
-    context: 2,
-    byte: ['0x90', '0xAB']
-  },
-  {
-    name: 'Hydra One',
-    shard: 'zone-2-0',
-    context: 2,
-    byte: ['0xAC', '0xC7']
-  },
-  {
-    name: 'Hydra Two',
-    shard: 'zone-2-1',
-    context: 2,
-    byte: ['0xC8', '0xE3']
-  },
-  {
-    name: 'Hydra Three',
-    shard: 'zone-2-2',
-    context: 2,
-    byte: ['0xE4', '0xFF']
-  }
+    {
+        name: 'Cyprus One',
+        shard: 'zone-0-0',
+        context: 2,
+        byte: ['0x00', '0x1D']
+    },
+    {
+        name: 'Cyprus Two',
+        shard: 'zone-0-1',
+        context: 2,
+        byte: ['0x1E', '0x3A']
+    },
+    {
+        name: 'Cyprus Three',
+        shard: 'zone-0-2',
+        context: 2,
+        byte: ['0x3B', '0x57']
+    },
+    {
+        name: 'Paxos One',
+        shard: 'zone-1-0',
+        context: 2,
+        byte: ['0x58', '0x73']
+    },
+    {
+        name: 'Paxos Two',
+        shard: 'zone-1-1',
+        context: 2,
+        byte: ['0x74', '0x8F']
+    },
+    {
+        name: 'Paxos Three',
+        shard: 'zone-1-2',
+        context: 2,
+        byte: ['0x90', '0xAB']
+    },
+    {
+        name: 'Hydra One',
+        shard: 'zone-2-0',
+        context: 2,
+        byte: ['0xAC', '0xC7']
+    },
+    {
+        name: 'Hydra Two',
+        shard: 'zone-2-1',
+        context: 2,
+        byte: ['0xC8', '0xE3']
+    },
+    {
+        name: 'Hydra Three',
+        shard: 'zone-2-2',
+        context: 2,
+        byte: ['0xE4', '0xFF']
+    }
 ]
 
 const externalShards = QUAI_CONTEXTS.filter((shard) => shard.shard !== selectedZone)
 const selectedShard = QUAI_CONTEXTS.find((shard) => shard.shard === selectedZone)
 
-async function transact (wallet) {
-  let nonce = await provider.getTransactionCount(wallet.address, 'pending')
-  while (true) {
-    const raw = await genRawTransaction(wallet, nonce)
-    const signed = await wallet.signTransaction(raw)
-    if (memPoolSize < memPoolMax) await sendRawTransaction(providerUrl, signed)
-    await sleep(interval)
-    if (nonce % 100 === 0) {
-      nonce = await provider.getTransactionCount(wallet.address, 'pending')
-    } else {
-      nonce++
+async function transact(wallet) {
+    let nonce = await provider.getTransactionCount(wallet.address, 'pending')
+    while (true) {
+        const raw = await genRawTransaction(wallet, nonce)
+        const signed = await wallet.signTransaction(raw)
+        if (memPoolSize < memPoolMax) await sendRawTransaction(providerUrl, signed)
+        await sleep(interval)
+        if (nonce % 100 === 0) {
+            nonce = await provider.getTransactionCount(wallet.address, 'pending')
+        } else {
+            nonce++
+        }
     }
-  }
 }
 
 ;(async () => {
-  logger.info('Starting QUAI load test', selectedShard.shard, selectedGroup)
+    logger.info('Starting QUAI load test', selectedShard.shard, selectedGroup)
 
-  const wallets = walletsJson[selectedGroup][selectedZone].map((wallet) => new Wallet(wallet.privateKey, provider))
-  memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
-  feeData = await provider.getFeeData()
-
-  const start = Date.now()
-  latest = start
-
-  setInterval(async () => {
+    const wallets = walletsJson[selectedGroup][selectedZone].map((wallet) => new Wallet(wallet.privateKey, provider))
     memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
-    if (memPoolSize > memPoolMax) logger.warn('mempool full')
-  }, 3000)
-
-  let integralError = 0;
-  const proportionalGain = 0.1; // These are tuning parameters that you'll need to adjust for your use case
-  const integralGain = 0.01;
-
-  setInterval(async () => {
-    const tps = transactions / ((Date.now() - latest) / 1000);
-    transactions = 0;
-    latest = Date.now();
-    const error = desiredTps - tps; // Calculate the error
-    integralError += error; // Accumulate the error over time
-    const controllerOutput = proportionalGain * error + integralGain * integralError; // PI controller output
-    interval -= controllerOutput; // Use controller output to adjust interval
-    if (interval < 0) { // Ensure interval never goes below 0
-      interval = 0;
-    }
-    logger.info(`tps: ${tps}, desiredTps: ${desiredTps}, interval: ${interval}`);
-  }, 30000);
-
-  setInterval(async () => {
     feeData = await provider.getFeeData()
-  }, 1000*30)
 
-  setInterval(async () => {
-    desiredTps += 3.33
-    if (desiredTps > 50) desiredTps = 50
-  }, 1000 * 60 * 60 * 1)
+    const start = Date.now()
+    latest = start
 
-  await Promise.map(wallets, async (wallet) => transact(wallet))
+    setInterval(async () => {
+        memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
+        if (memPoolSize > memPoolMax) logger.warn('mempool full')
+    }, 3000)
+
+    setInterval(async () => {
+        const tps = transactions / ((Date.now() - latest) / 1000);
+        transactions = 0;
+        latest = Date.now();
+        if (tps < desiredTps) {
+            interval -= 10 // should be based on desiredTps - tps
+        } else {
+            interval += 10
+        }
+        logger.info(`tps: ${tps}, desiredTps: ${desiredTps}, interval: ${interval}`);
+    }, 30000);
+
+    setInterval(async () => {
+        feeData = await provider.getFeeData()
+    }, 1000 * 30)
+
+    setInterval(async () => {
+        desiredTps += 3.33
+        if (desiredTps > 50) desiredTps = 50
+    }, 1000 * 60 * 60 * 1)
+
+    await Promise.map(wallets, async (wallet) => transact(wallet))
 })()
