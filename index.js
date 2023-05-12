0eaf43357f8924ee26fa7e83e9d8fb484303de30
Connor Hubbard <connor@dominantstrategies.io>
2023-05-12 10:48:43 -0500

    cleaned the lint

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Promise = require('bluebird')
-const {JsonRpcProvider, Wallet} = require('quais')
+const { JsonRpcProvider, Wallet } = require('quais')
 const walletsJson = require('./wallets.json')
-const {info, warn } = require('./logger')
-const {getRandomAddressInShard, lookupTxPending, nodeData, sleep,sendRawTransaction, QUAI_CONTEXTS} = require('./utils')
-const {post} = require("axios");
+const { info, warn } = require('./logger')
+const { getRandomAddressInShard, lookupTxPending, nodeData, sleep, sendRawTransaction, QUAI_CONTEXTS } = require('./utils')
 
 const host = 'localhost'
 const protocol = 'http'
@@ -18,7 +17,7 @@ const etxFreq = 0.2
 const provider = new JsonRpcProvider(providerUrl)
 const memPoolMax = 9000
 
-let memPoolSize;
+let memPoolSize
 let transactions = 0
 let latest
 let interval = 4000
@@ -28,102 +27,99 @@ let feeData
 const externalShards = QUAI_CONTEXTS.filter((shard) => shard.shard !== selectedZone)
 const selectedShard = QUAI_CONTEXTS.find((shard) => shard.shard === selectedZone)
 
-function getRandomExternalAddress() {
-    return getRandomAddressInShard(externalShards[Math.floor(Math.random() * externalShards.length)])
+function getRandomExternalAddress () {
+  return getRandomAddressInShard(externalShards[Math.floor(Math.random() * externalShards.length)])
 }
 
-function getRandomInternalAddress() {
-    return getRandomAddressInShard(selectedShard)
+function getRandomInternalAddress () {
+  return getRandomAddressInShard(selectedShard)
 }
 
-async function genRawTransaction(wallet, nonce) {
-    const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
-    const isExternal = Math.random() < etxFreq
+async function genRawTransaction (wallet, nonce) {
+  const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
+  const isExternal = Math.random() < etxFreq
 
-    let to, type
+  let to, type
 
-    if (isExternal) { // is external this time
-        to = getRandomExternalAddress()
-        type = 2
+  if (isExternal) { // is external this time
+    to = getRandomExternalAddress()
+    type = 2
+  } else {
+    to = getRandomInternalAddress()
+    type = 0
+  }
+
+  const ret = {
+    to,
+    value,
+    nonce,
+    gasLimit: 42000,
+    maxFeePerGas: feeData.maxFeePerGas * BigInt(2),
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    type,
+    chainId
+  }
+  if (isExternal) { // is external this time
+    ret.externalGasLimit = BigInt(100000)
+    ret.externalGasPrice = feeData.maxFeePerGas * BigInt(2)
+    ret.externalGasTip = feeData.maxPriorityFeePerGas * BigInt(2)
+  }
+  return ret
+}
+
+async function transact (wallet) {
+  let nonce = await provider.getTransactionCount(wallet.address, 'pending')
+  while (true) {
+    const raw = await genRawTransaction(wallet, nonce)
+    const signed = await wallet.signTransaction(raw)
+    if (memPoolSize < memPoolMax) {
+      transactions++
+      await sendRawTransaction(providerUrl, signed)
+    }
+    await sleep(interval)
+    if (nonce % 100 === 0) {
+      nonce = await provider.getTransactionCount(wallet.address, 'pending')
     } else {
-        to = getRandomInternalAddress()
-        type = 0
+      nonce++
     }
-
-    const ret = {
-        to,
-        value,
-        nonce,
-        gasLimit: 42000,
-        maxFeePerGas: feeData.maxFeePerGas * BigInt(2),
-        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
-        type,
-        chainId
-    }
-    if (isExternal) { // is external this time
-        ret.externalGasLimit = BigInt(100000)
-        ret.externalGasPrice = feeData.maxFeePerGas * BigInt(2)
-        ret.externalGasTip = feeData.maxPriorityFeePerGas * BigInt(2)
-    }
-    return ret
-}
-
-
-
-
-async function transact(wallet) {
-    let nonce = await provider.getTransactionCount(wallet.address, 'pending')
-    while (true) {
-        const raw = await genRawTransaction(wallet, nonce)
-        const signed = await wallet.signTransaction(raw)
-        if (memPoolSize < memPoolMax) {
-            transactions++
-            await sendRawTransaction(providerUrl, signed)
-        }
-        await sleep(interval)
-        if (nonce % 100 === 0) {
-            nonce = await provider.getTransactionCount(wallet.address, 'pending')
-        } else {
-            nonce++
-        }
-    }
+  }
 }
 
 ;(async () => {
-    info('Starting QUAI load test', {shard: selectedShard.shard, selectedGroup})
+  info('Starting QUAI load test', { shard: selectedShard.shard, selectedGroup })
 
-    const wallets = walletsJson[selectedGroup][selectedZone].map((wallet) => new Wallet(wallet.privateKey, provider))
+  const wallets = walletsJson[selectedGroup][selectedZone].map((wallet) => new Wallet(wallet.privateKey, provider))
+  memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
+  feeData = await provider.getFeeData()
+
+  const start = Date.now()
+  latest = start
+
+  setInterval(async () => {
     memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
+    if (memPoolSize > memPoolMax) warn('mempool full')
+  }, 1000 * 3)
+
+  setInterval(async () => {
+    const tps = transactions / ((Date.now() - latest) / 1000)
+    transactions = 0
+    latest = Date.now()
+    if (tps < desiredTps) {
+      interval -= 10 // should be based on desiredTps - tps
+    } else {
+      interval += 10
+    }
+    info('tps check', { tps, desiredTps, interval })
+  }, 1000 * 30)
+
+  setInterval(async () => {
     feeData = await provider.getFeeData()
+  }, 1000 * 30)
 
-    const start = Date.now()
-    latest = start
+  setInterval(async () => {
+    desiredTps += 3.33
+    if (desiredTps > 50) desiredTps = 50
+  }, 1000 * 60 * 60 * 1)
 
-    setInterval(async () => {
-        memPoolSize = Math.max(...(await lookupTxPending(providerUrl)))
-        if (memPoolSize > memPoolMax) warn('mempool full')
-    }, 1000 * 3)
-
-    setInterval(async () => {
-        const tps = transactions / ((Date.now() - latest) / 1000);
-        transactions = 0;
-        latest = Date.now();
-        if (tps < desiredTps) {
-            interval -= 10 // should be based on desiredTps - tps
-        } else {
-            interval += 10
-        }
-        info('tps check', {tps, desiredTps, interval})
-    }, 1000 * 30);
-
-    setInterval(async () => {
-        feeData = await provider.getFeeData()
-    }, 1000 * 30)
-
-    setInterval(async () => {
-        desiredTps += 3.33
-        if (desiredTps > 50) desiredTps = 50
-    }, 1000 * 60 * 60 * 1)
-
-    await Promise.map(wallets, transact)
+  await Promise.map(wallets, transact)
 })()
