6259bfa868308035e3561ddf6823994dd7be08b8
Connor Hubbard <connor@dominantstrategies.io>
2023-05-22 12:27:08 -0500

    1hr interval + 1000 starting tps

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let latest
 const interval = 10000
 let feeData
 let walletStart = 0
-let walletEnd = 80
+let walletEnd = 160
 
 const generateAbsoluteRandomRatio = 0
 
@@ -141,7 +141,7 @@ async function transact (wallet) {
     walletEnd += 40
     const newWallets = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => new Wallet(wallet.privateKey, provider))
     await Promise.map(newWallets, transact)
-  }, 1000 * 60 * 60 / 10)
+  }, 1000 * 60 * 60 * 1)
 
   await Promise.map(wallets, transact)
 })()
