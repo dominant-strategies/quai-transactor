da614e1500de451c3969d7d28e0e25026595e5cb
Connor Hubbard <connor@dominantstrategies.io>
2023-05-25 16:45:48 -0500

    cleaned lint

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const Promise = require('bluebird')
-const {JsonRpcProvider, Wallet} = require('quais')
+const { JsonRpcProvider, Wallet } = require('quais')
 const walletsJson = require('./wallets.json')
-const {info, warn, error} = require('./logger')
+const { info, warn, error } = require('./logger')
 const {
-    generateRandomAddressInShard,
-    lookupTxPending,
-    nodeData,
-    sleep,
-    sendRawTransaction,
-    QUAI_CONTEXTS
+  generateRandomAddressInShard,
+  lookupTxPending,
+  nodeData,
+  sleep,
+  sendRawTransaction,
+  QUAI_CONTEXTS
 } = require('./utils')
 
 const protocol = 'http'
@@ -38,122 +38,122 @@ const generateAbsoluteRandomRatio = 0
 const externalShards = QUAI_CONTEXTS.filter((shard) => shard.shard !== selectedZone)
 const selectedShard = QUAI_CONTEXTS.find((shard) => shard.shard === selectedZone)
 
-function getRandomExternalAddress() {
-    const randomZone = externalShards[Math.floor(Math.random() * externalShards.length)]
-    if (Math.random() < generateAbsoluteRandomRatio) {
-        return generateRandomAddressInShard(randomZone)
-    }
-    const addresses = walletsJson[selectedGroup][randomZone.shard].slice(walletStart, walletEnd).map((wallet) => wallet.address)
-    return addresses[Math.floor(Math.random() * addresses.length)]
+function getRandomExternalAddress () {
+  const randomZone = externalShards[Math.floor(Math.random() * externalShards.length)]
+  if (Math.random() < generateAbsoluteRandomRatio) {
+    return generateRandomAddressInShard(randomZone)
+  }
+  const addresses = walletsJson[selectedGroup][randomZone.shard].slice(walletStart, walletEnd).map((wallet) => wallet.address)
+  return addresses[Math.floor(Math.random() * addresses.length)]
 }
 
-function getRandomInternalAddress() {
-    if (Math.random() < generateAbsoluteRandomRatio) {
-        return generateRandomAddressInShard(selectedShard)
-    }
-    const addresses = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => wallet.address)
-    return addresses[Math.floor(Math.random() * addresses.length)]
+function getRandomInternalAddress () {
+  if (Math.random() < generateAbsoluteRandomRatio) {
+    return generateRandomAddressInShard(selectedShard)
+  }
+  const addresses = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => wallet.address)
+  return addresses[Math.floor(Math.random() * addresses.length)]
 }
 
-async function genRawTransaction(wallet, nonce) {
-    const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
-    const isExternal = Math.random() < etxFreq
+async function genRawTransaction (wallet, nonce) {
+  const value = Math.floor(Math.random() * (hiValue - loValue + 1) + loValue)
+  const isExternal = Math.random() < etxFreq
 
-    let to, type
+  let to, type
 
-    if (isExternal) { // is external this time
-        to = getRandomExternalAddress()
-        type = 2
-    } else {
-        to = getRandomInternalAddress()
-        type = 0
-    }
+  if (isExternal) { // is external this time
+    to = getRandomExternalAddress()
+    type = 2
+  } else {
+    to = getRandomInternalAddress()
+    type = 0
+  }
 
-    const ret = {
-        to,
-        value,
-        nonce,
-        gasLimit: 42000,
-        maxFeePerGas: feeData.maxFeePerGas * BigInt(2),
-        maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
-        type,
-        chainId
-    }
-    if (isExternal) { // is external this time
-        ret.externalGasLimit = BigInt(100000)
-        ret.externalGasPrice = feeData.maxFeePerGas * BigInt(2)
-        ret.externalGasTip = feeData.maxPriorityFeePerGas * BigInt(2)
-    }
-    return ret
+  const ret = {
+    to,
+    value,
+    nonce,
+    gasLimit: 42000,
+    maxFeePerGas: feeData.maxFeePerGas * BigInt(2),
+    maxPriorityFeePerGas: feeData.maxPriorityFeePerGas,
+    type,
+    chainId
+  }
+  if (isExternal) { // is external this time
+    ret.externalGasLimit = BigInt(100000)
+    ret.externalGasPrice = feeData.maxFeePerGas * BigInt(2)
+    ret.externalGasTip = feeData.maxPriorityFeePerGas * BigInt(2)
+  }
+  return ret
 }
 
-async function transact(wallet) {
-    await sleep(interval * Math.random())
-    let nonce = await provider.getTransactionCount(wallet.address, 'pending')
-    let backoff = 0
-    while (true) {
-        const raw = await genRawTransaction(wallet, nonce)
-        const signed = await wallet.signTransaction(raw)
-        if (memPoolSize < memPoolMax) {
-            transactions++
-            try {
-                info('sending transaction', {memPoolSize, nonce, ...feeData, address: wallet.address})
-                await sendRawTransaction(providerUrl, signed)
-            } catch (e) {
-                error('error sending transaction', e)
-                if (e.message === 'intrinsic gas too low') {
-                    feeData = await provider.getFeeData()
-                }
-                if (!['replacement transaction underpriced', 'nonce too low'].includes(e.message)) {
-                    await sleep(interval * Math.pow(1.1, backoff++))
-                    continue
-                }
-            }
-            nonce++
-            backoff = 0
+async function transact (wallet) {
+  await sleep(interval * Math.random())
+  let nonce = await provider.getTransactionCount(wallet.address, 'pending')
+  let backoff = 0
+  while (true) {
+    const raw = await genRawTransaction(wallet, nonce)
+    const signed = await wallet.signTransaction(raw)
+    if (memPoolSize < memPoolMax) {
+      transactions++
+      try {
+        info('sending transaction', { memPoolSize, nonce, ...feeData, address: wallet.address })
+        await sendRawTransaction(providerUrl, signed)
+      } catch (e) {
+        error('error sending transaction', e)
+        if (e.message === 'intrinsic gas too low') {
+          feeData = await provider.getFeeData()
         }
-        await sleep(interval)
+        if (!['replacement transaction underpriced', 'nonce too low'].includes(e.message)) {
+          await sleep(interval * Math.pow(1.1, backoff++))
+          continue
+        }
+      }
+      nonce++
+      backoff = 0
     }
+    await sleep(interval)
+  }
 }
 
 ;(async () => {
-    info('Starting QUAI load test', {shard: selectedShard.shard, selectedGroup})
-    if (walletEnd > walletsJson[selectedGroup][selectedZone].length) {
-        walletEnd = walletsJson[selectedGroup][selectedZone].length
-        info('walletEnd is greater than the number of wallets in the group, setting walletEnd to the number of wallets in the group', {walletEnd})
-    }
+  info('Starting QUAI load test', { shard: selectedShard.shard, selectedGroup })
+  if (walletEnd > walletsJson[selectedGroup][selectedZone].length) {
+    walletEnd = walletsJson[selectedGroup][selectedZone].length
+    info('walletEnd is greater than the number of wallets in the group, setting walletEnd to the number of wallets in the group', { walletEnd })
+  }
 
-    const wallets = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => new Wallet(wallet.privateKey, provider))
-    memPoolSize = (await lookupTxPending(providerUrl))[0]
+  const wallets = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => new Wallet(wallet.privateKey, provider))
+  memPoolSize = (await lookupTxPending(providerUrl))[0]
+  feeData = await provider.getFeeData()
+
+  const start = Date.now()
+  latest = start
+
+  setInterval(async () => {
+    memPoolSize = (await lookupTxPending(providerUrl))?.[0] || memPoolSize
+    if (memPoolSize > memPoolMax) warn('mempool full')
+  }, 1000 * 3)
+
+  setInterval(async () => {
+    const tps = transactions / ((Date.now() - latest) / 1000)
+    transactions = 0
+    latest = Date.now()
+    info('tps check', { tps, walletEnd })
+  }, 1000 * 30)
+
+  setInterval(async () => {
     feeData = await provider.getFeeData()
+  }, 1000 * 30)
 
-    const start = Date.now()
-    latest = start
+  setInterval(async () => {
+    if (walletEnd + numberOfNewWallets <= walletsJson[selectedGroup][selectedZone].length) {
+      walletStart = walletEnd
+      walletEnd += numberOfNewWallets
+      const newWallets = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => new Wallet(wallet.privateKey, provider))
+      await Promise.map(newWallets, transact)
+    }
+  }, 1000 * 60 * 60 * 1)
 
-    setInterval(async () => {
-        memPoolSize = (await lookupTxPending(providerUrl))?.[0] || memPoolSize
-        if (memPoolSize > memPoolMax) warn('mempool full')
-    }, 1000 * 3)
-
-    setInterval(async () => {
-        const tps = transactions / ((Date.now() - latest) / 1000)
-        transactions = 0
-        latest = Date.now()
-        info('tps check', {tps, walletEnd})
-    }, 1000 * 30)
-
-    setInterval(async () => {
-        feeData = await provider.getFeeData()
-    }, 1000 * 30)
-
-    setInterval(async () => {
-	if (walletEnd + numberOfNewWallets <= walletsJson[selectedGroup][selectedZone].length) {
-          walletStart = walletEnd
-          walletEnd += numberOfNewWallets
-          const newWallets = walletsJson[selectedGroup][selectedZone].slice(walletStart, walletEnd).map((wallet) => new Wallet(wallet.privateKey, provider))
-          await Promise.map(newWallets, transact)
-	}
-    }, 1000 * 60 * 60 * 1)
-
-    await Promise.map(wallets, transact)
+  await Promise.map(wallets, transact)
 })()
