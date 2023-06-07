c1c48c6818d0fd12fa6f4a79a916842cec970e7e
Connor Hubbard <connor@dominantstrategies.io>
2023-06-07 14:26:36 -0500

    updated the galena config

--- config/galena.js
+++ config/galena.js
@@ -3,4 +3,14 @@
 module.exports = {
   env: 'galena',
   blockTime: 12 * 1000 // 12s
+  txs: {
+    tps: {
+      walletEnd: 100,
+      increment: {
+        amount: 44,
+        interval: 1000 * 60 * 60 * 2 // 2hr
+      },
+    },
+    etxFreq: .1,
+  },
 }
