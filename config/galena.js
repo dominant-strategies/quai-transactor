2d9f192e89d9a7456d2118bf276bf027a03526f4
Connor Hubbard <connor@dominantstrategies.io>
2023-06-08 16:47:20 -0500

    updated galena tps

--- config/galena.js
+++ config/galena.js
@@ -3,9 +3,12 @@
 module.exports = {
   env: 'galena',
   blockTime: 12 * 1000, // 12s
+  machinesRunning: 4,
   txs: {
     tps: {
+      target: 300,
       increment: {
+	amount: 100,
         interval: 1000 * 60 * 60 * 2 // 2hr
       }
     },
