f0585aa345182e09bac9ff7f1c32a6016cf14aa7
Connor Hubbard <connor@dominantstrategies.io>
2023-06-09 12:45:08 -0500

    up'd orchard tps to 1000

--- config/orchard.js
+++ config/orchard.js
@@ -6,8 +6,9 @@ module.exports = {
   machinesRunning: 6,
   txs: {
     tps: {
-      target: 800,
+      target: 1000,
       increment: {
+	enabled: false,
         amount: 300,
         interval: 1000 * 60 * 60 * 4 // 6hr
       }
