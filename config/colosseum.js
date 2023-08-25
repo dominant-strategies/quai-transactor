764751d726425579edab8903dc422d8a9f2884cf
Hussam <hussam@dominantstrategies.io>
2023-08-25 12:19:32 -0500

    colosseum configs

--- config/colosseum.js
+++ config/colosseum.js
@@ -3,14 +3,17 @@
 module.exports = {
   env: 'colosseum',
   blockTime: 12 * 1000, // 10s
-  machinesRunning: 3,
+  machinesRunning: 4,
   txs: {
     tps: {
-      target: 200,
+      target: 2000,
       increment: {
         enabled: false
       }
     },
-    etxFreq: 0.2
+    etxFreq: 0.1
+  },
+  memPool: {
+    max: 12000
   }
 }
