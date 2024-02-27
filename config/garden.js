51b5246ff7854ed9dcd5ede93fc181d21d1bed99
gop <gameofpointers@proton.me>
2024-02-27 12:08:23 -0600

    Updated the garden config

--- config/garden.js
+++ config/garden.js
@@ -1,19 +1,52 @@
 'use strict'
 
+const path = require('path')
+const pkg = require(path.join(__dirname, '..', 'package.json'))
+
 module.exports = {
   env: 'garden',
-  blockTime: 7 * 1000, // 7s
-  machinesRunning: 2,
+  dumpConfig: true,
+  blockTime: 2 * 1000, // 2s
+  machinesRunning: 3,
+  numSlices: 9,
+  log: {
+    winston: {
+      opts: {
+        name: pkg.name,
+        version: pkg.version,
+        level: 'info'
+      }
+    }
+  },
   txs: {
     tps: {
-      target: 2000,
+      target: 10,
       increment: {
-        enabled: false
+        enabled: true,
+        amount: 10,
+        interval: 1000 * 60 * 30 // 30 mins
+      },
+      check: {
+        enabled: true,
+        interval: 1000 * 3 // 3s
       }
     },
-    etxFreq: 0.01,
+    etxFreq: 0.1,
+    loValue: 1,
+    hiValue: 100,
+    absoluteRandomAddressRatio: 0
   },
   memPool: {
-    max: 12000,
+    max: 4096,
+    check: {
+      enabled: true,
+      interval: 1000 * 3 // 3s
+    }
+  },
+  feeData: {
+    check: {
+      enabled: true,
+      interval: 1000 * 1 // 3s
+    }
   }
 }
