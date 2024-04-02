d6012fbb6c8d1f36fc0eebf0c1f31e5301dc43a6
DenisIvanov26 <denis.ivanov26@gmail.com>
2024-04-02 12:04:29 -0500

    fix type 2 tx

--- index.js
+++ index.js
@@ -99,10 +99,11 @@ async function genRawTransaction (nonce, double) {
     chainId
   }
   if (isExternal) { // is external this time
-    ret.externalGasLimit = BigInt(42000)
-    ret.externalGasPrice = feeData.maxFeePerGas * BigInt(9) * (double ? BigInt(2) : BigInt(1))
-    ret.externalGasTip = feeData.maxPriorityFeePerGas * BigInt(9) * (double ? BigInt(2) : BigInt(1))
+    ret.externalGasLimit = BigInt(42000);
+    ret.externalGasPrice = BigInt(feeData.maxFeePerGas) * BigInt(9) * (double ? BigInt(2) : BigInt(1));
+    //ret.externalGasTip = BigInt(feeData.maxPriorityFeePerGas) * BigInt(9) * (double ? BigInt(2) : BigInt(1));
   }
+  
   return ret
 }
 
